f694aa4915f7a7811741f4dcbaece95603a941cd
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2023-12-25 15:03:53 +0200

    Create 04.Biggestof3Numbers.js

---
+++ Data Types and Variables /More Exercise/04.Biggestof3Numbers.js
@@ -0,0 +1,28 @@
+function biggestofThree( a , b, c)
+{
+
+   let theBiggestofThree = 0
+
+   if( a > b)
+   {
+    theBiggestofThree += a
+   }else
+   {
+    theBiggestofThree +=b
+   }
+
+   if(theBiggestofThree < c )
+   {
+    theBiggestofThree = c
+   }
+
+
+   console.log(theBiggestofThree)
+
+
+
+
+
+
+
+}
