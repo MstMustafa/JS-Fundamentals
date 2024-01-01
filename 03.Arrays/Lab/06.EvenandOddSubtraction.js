30954dd9acf0dccad975bcefb1487c6fb215dc47
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-01-01 15:30:06 +0200

    Create 06.EvenandOddSubtraction.js

---
+++ 03.Arrays/Lab/06.EvenandOddSubtraction.js
@@ -0,0 +1,18 @@
+function calculateEvenOddDifference(numbers) {
+  let sumEven = 0;
+  let sumOdd = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    const number = Number(numbers[i]);
+
+    // Check if the number is even or odd
+    if (number % 2 === 0) {
+      sumEven += number;
+    } else {
+      sumOdd += number;
+    }
+  }
+
+  const difference = sumEven - sumOdd;
+  console.log(difference);
+}
