3f1618337c620a005d867be30a2d126d0248fa56
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-01-22 16:41:53 +0200

    Create 05.SimpleCalculator.js

---
+++ 04.Functions/Lab/05.SimpleCalculator.js
@@ -0,0 +1,23 @@
+function calculate(numOne, numTwo, operator) {
+  let result;
+
+  switch (operator) {
+    case 'multiply':
+      result = numOne * numTwo;
+      break;
+    case 'divide':
+      result = numOne / numTwo;
+      break;
+    case 'add':
+      result = numOne + numTwo;
+      break;
+    case 'subtract':
+      result = numOne - numTwo;
+      break;
+    default:
+      console.log('Invalid operator');
+      return;
+  }
+
+  console.log(result);
+}
