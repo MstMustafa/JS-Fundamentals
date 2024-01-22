44ac236536e087d0fb3eb9344485a3ed68913f81
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-01-22 16:43:38 +0200

    Create 06.SignCheck.js

---
+++ 04.Functions/Lab/06.SignCheck.js
@@ -0,0 +1,22 @@
+function checkSign(numOne, numTwo, numThree) {
+  
+  let negativeCount = 0;
+
+  if (numOne < 0) {
+    negativeCount++;
+  }
+
+  if (numTwo < 0) {
+    negativeCount++;
+  }
+
+  if (numThree < 0) {
+    negativeCount++;
+  }
+
+  if (negativeCount % 2 === 0) {
+    console.log("Positive");
+  } else {
+    console.log("Negative");
+  }
+}
