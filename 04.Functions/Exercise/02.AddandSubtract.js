8184fd4033e30f2d02b41b58f4959d3d2125e1f8
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-01-23 16:04:56 +0200

    Create 02.AddandSubtract.js

---
+++ 04.Functions/Exercise/02.AddandSubtract.js
@@ -0,0 +1,10 @@
+function calculateAndLogResult(num1, num2, num3) {
+    
+    let resultOfSum = num1 + num2;
+
+    
+    let finalResult = resultOfSum - num3;
+
+    
+    console.log(finalResult);
+}
