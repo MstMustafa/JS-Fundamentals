77b34e2857c60a2df195af9deec89bd541572e4a
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-01-24 16:10:41 +0200

    Create 04.OddAndEvenSum.js

---
+++ 04.Functions/Exercise/04.OddAndEvenSum.js
@@ -0,0 +1,18 @@
+function sumEvenOddDigits(number) {
+
+    let evenSum = 0;
+    let oddSum = 0;
+
+    let numStr = number.toString();
+
+    for (let i = 0; i < numStr.length; i++) {
+        let digit = parseInt(numStr[i]);
+
+        if (digit % 2 === 0) {
+            evenSum += digit;
+        } else {
+            oddSum += digit;
+        }
+    }
+    console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
+}
