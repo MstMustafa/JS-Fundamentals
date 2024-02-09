a15ee73615fa51d5b31f95b6e9deef66d34094c1
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-02-09 23:36:13 +0200

    Create 01.PersonInfo.js

---
+++ Objects and Classes/Lab/01.PersonInfo.js
@@ -0,0 +1,12 @@
+function personInfo(firstName , secondName , age)
+{
+    let personalInfo = {
+        firstName : firstName,
+        lastName : secondName,
+        age : age
+
+    }
+
+  return personalInfo
+
+}
