e68fb7e5fe8a41f4c73187aad6bd6a2d91913f06
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-02-29 15:20:58 +0200

    Create 02.OddOccurrences.js

---
+++ 07.Associative Arrays/Exercise/02.OddOccurrences.js
@@ -0,0 +1,16 @@
+function oddOccurrences(sentence) {
+    const words = sentence.toLowerCase().split(' ');
+    
+    const wordCounts = {};
+    words.forEach(word => {
+        if (wordCounts[word]) {
+            wordCounts[word] += 1;
+        } else {
+            wordCounts[word] = 1;
+        }
+    });
+    
+    const oddWords = Object.keys(wordCounts).filter(word => wordCounts[word] % 2 !== 0);
+    
+    return oddWords.join(' ');
+}
