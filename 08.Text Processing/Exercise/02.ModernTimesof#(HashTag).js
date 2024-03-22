f2b8022c16d11b0875f99fa3e5a74059d2e14a9b
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-03-22 12:36:26 +0200

    Update 02.ModernTimesof#(HashTag).js

--- 08.Text Processing/Exercise/02.ModernTimesof#(HashTag).js
+++ 08.Text Processing/Exercise/02.ModernTimesof#(HashTag).js
@@ -1,16 +1,24 @@
 function findAndPrintHashTags(inputString) {
-   
-    const hashTags = inputString.match(/#\w+/g);
-    
-    if (hashTags) {
-       
-        hashTags.forEach(tag => {
-            
-            const word = tag.slice(1);
-            if (/^[A-Za-z]+$/.test(word)) {
-               
-                console.log(word);
-            }
-        });
+ 
+  const words = inputString.split(' ');
+  
+  words.forEach(word => {
+
+    if (word.startsWith('#') && word.length > 1) {
+      const potentialTag = word.substring(1);
+      
+      let isValid = true;
+      for (let i = 0; i < potentialTag.length; i++) {
+        const char = potentialTag.charCodeAt(i);
+        if (!((char >= 65 && char <= 90) || (char >= 97 && char <= 122))) {
+          isValid = false;
+          break;
+        }
+      }
+      
+      if (isValid) {
+        console.log(potentialTag);
+      }
     }
+  });
 }
