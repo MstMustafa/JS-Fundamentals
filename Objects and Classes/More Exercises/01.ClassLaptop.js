4054dac0ca35845554741207bee44173aa109f0d
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-05-15 21:25:32 +0300

    Update 01.ClassLaptop.js

--- Objects and Classes/More Exercises/01.ClassLaptop.js
+++ Objects and Classes/More Exercises/01.ClassLaptop.js
@@ -1,25 +1,25 @@
 class Laptop {
     constructor(info, quality) {
-        this.info = info; // Object containing producer, age, and brand
-        this.isOn = false; // Boolean, false by default
+        this.info = info; 
+        this.isOn = false; 
         this.quality = quality; // Number
     }
 
     turnOn() {
         this.isOn = true;
-        this.quality -= 1; // Decrease quality by 1
+        this.quality -= 1; 
     }
 
     turnOff() {
         this.isOn = false;
-        this.quality -= 1; // Decrease quality by 1
+        this.quality -= 1; 
     }
 
     showInfo() {
-        return JSON.stringify(this.info); // Return info as JSON
+        return JSON.stringify(this.info); 
     }
 
     get price() {
-        return 800 - (this.info.age * 2) + (this.quality * 0.5); // Calculate price
+        return 800 - (this.info.age * 2) + (this.quality * 0.5);
     }
 }
