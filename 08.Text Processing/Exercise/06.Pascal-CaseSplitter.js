9d4972f32c6e6b48313fd6dfd3ec46e1e64e42ce
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-05-16 20:44:09 +0300

    Update 06.Pascal-CaseSplitter.js

--- 08.Text Processing/Exercise/06.Pascal-CaseSplitter.js
+++ 08.Text Processing/Exercise/06.Pascal-CaseSplitter.js
@@ -5,13 +5,13 @@ function pascalCaseSplitter(inputString) {
   }
 
   let words = [];
-  let startOfWord = 0; // Index where the current word starts
+  let startOfWord = 0;
 
   for (let i = 1; i < inputString.length; i++) {
     
     if (inputString[i] === inputString[i].toUpperCase()) {
       words.push(inputString.substring(startOfWord, i));
-      startOfWord = i; // Update the startOfWord to the current character's index
+      startOfWord = i;
     }
   }
  
