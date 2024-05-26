19d4ec18c0e1f4ea70168a49ed8d5a86660f11f2
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-05-26 10:58:42 +0300

    Update 02.DistinctArray.js

--- 05.Arrays Advanced/Exercise/02.DistinctArray.js
+++ 05.Arrays Advanced/Exercise/02.DistinctArray.js
@@ -3,7 +3,7 @@ function removeDuplicatesAndPrint(arr) {
 
     for (let i = 0; i < arr.length; i++) {
         if (uniqueElements.indexOf(arr[i]) === -1) {
-            uniqueElements.push(arr[i]); // Add unique elements to the uniqueElements array
+            uniqueElements.push(arr[i]);
         }
     }
 
